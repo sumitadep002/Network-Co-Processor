a6afcd58519c3f642b93ba9ce027c08cf1ee5756
Sumit Adep <adepsumit123@gmail.com>
2026-02-15 13:09:39 +0530

    init wifi

--- project/main/main.c
+++ project/main/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 
+#include "wifi.h"
+
 void app_main(void)
 {
+    wifi_st_init("Sumit", "123456789");
     while (1)
     {
         printf("Hello-World\n");
